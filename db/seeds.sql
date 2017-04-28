a383da112f808907b3821773d7db74add4317c9e
Kathy <kepile@verizon.net>
2017-04-27 20:35:20 -0400

    added schema seeds connection

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+USE burger_db;
+
+INSERT INTO burgers (burger_name, devoured) values
+('Double Bacon Cheeseburer', false),
+('Blue Cheese Grass Fed Burger', false),
+('Big Mac', false),
+('Bison Burger with cheddar cheese and pickled onions', false),
+('Bar-b-que beef burger', false),
+('Turkey burger with cheese', false),
+('Veggie Burger', false);
